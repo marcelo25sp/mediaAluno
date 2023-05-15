b367fca8cb787a0f74b557e0b48ff3282d5a66ba
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-15 19:40:59 -0300

    Adicionando funcionalidades

--- MediaAluno.java
+++ MediaAluno.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class MediaAluno {
 
 	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite o nome do usuário:");
+		String nome;
+		nome = teclado.next();
+		System.out.println("Bem vindo(a) " + nome);
 
 		
 		Scanner sc = new Scanner(System.in);
@@ -16,7 +22,7 @@ public class MediaAluno {
 	    nota1 = sc.nextInt();
 	    nota2 = sc.nextInt();
 	    media = (nota1 + nota2)/2;		
-	    System.out.println(media);
+	    System.out.println("A média do aluno é: " + media);
 	    
 	    if (media > 6) {
 	    	System.out.println("Aluno Aprovado");
